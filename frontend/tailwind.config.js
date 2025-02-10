b7c8dd25fee78f1604ada3c3292d5dc1fa97ad95
buyangdaffa <daffaeffendy2911@gmail.com>
2025-02-09 19:44:31 -0800

    setting up landing page

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+};
