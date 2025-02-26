5e4b57b621022198f8f0ce128a86c9529b367ec9
vangeliq <valeryangelique2001@gmail.com>
2025-02-26 13:32:04 -0800

    Fixed imports

--- frontend/src/app/GCalIntegration/page.js
+++ frontend/src/app/GCalIntegration/page.js
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './../components/Header';
+import Footer from './../components/Footer';
 import { CheckCircle } from 'lucide-react';
 
 const GCalIntegration = () => {
