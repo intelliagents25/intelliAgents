a76ed9f1cde5c4dda488a9753958a0e9c3575320
vangeliq <valeryangelique2001@gmail.com>
2025-03-02 16:32:37 -0800

    updated links

--- frontend/src/app/components/Header.js
+++ frontend/src/app/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <header className="h-[137px] px-[90px] pt-[21px] h- bg-gradient-to-b from-[#37b5ec] via-[#98d7f2] to-[#f9f9f9]">
       <nav className="mx-auto flex items-center justify-between">
-        <div className="text-4xl text-[#272776] font-bold">Intelli</div>
+        <div className="text-4xl text-[#272776] font-bold">
+          <Link href={{pathname: '/'}} className="text-4xl text-[#272776] font-bold">
+            Intelli
+          </Link>
+        </div>
         <div className="space-x-8">
           <a href="about-us" className="text-xl font-semibold text-black hover:text-gray-700">About Us</a>
-          <a href="FAQ" className="text-xl font-semibold text-black hover:text-gray-700">FAQ's</a>
+          <a href="faq" className="text-xl font-semibold text-black hover:text-gray-700">FAQ's</a>
           <a href="contact-us" className="text-xl font-semibold text-black hover:text-gray-700">Contact Us</a>
         </div>
       </nav>
