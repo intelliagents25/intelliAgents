79436468645b3ecb5358899fb421031a01281191
vangeliq <valeryangelique2001@gmail.com>
2025-03-05 01:30:22 -0800

    fixed alignment and added minimum wait time

--- frontend/src/app/components/chat/ChatRequests.js
+++ frontend/src/app/components/chat/ChatRequests.js
@@ -1,5 +1,8 @@
+const MIN_WAIT_TIME = 2000; // if it returns too fast it feels weird
+
 export const sendDataToBot = async (message_str) => {
     let data = {"message": message_str}
+    let start = new Date()
 
     return fetch("/api/chat", {
         method: 'POST',
@@ -18,5 +21,13 @@ export const sendDataToBot = async (message_str) => {
         })
         .catch((error) => {
             return error;
+        })
+        .then((response) => {
+            let end = new Date();
+            if (end - start < MIN_WAIT_TIME) {
+                return new Promise(resolve => setTimeout(() => resolve(response), MIN_WAIT_TIME - (end - start)));
+            } else {
+                return response;
+            }
         });
 };
