f0af406724c081f0dc9bb41266df98bd662f0b84
buyangdaffa <daffaeffendy2911@gmail.com>
2025-03-05 14:26:47 -0800

    responsive font size

--- frontend/src/app/components/HowItWorks.js
+++ frontend/src/app/components/HowItWorks.js
@@ -4,7 +4,7 @@ import { Icons } from './Icons';
 const HowItWorks = ({}) => {
   return (
     <div className="">
-      <button className="flex items-center justify-center w-[186px] h-[44px] border-[1px] border-[#2E374C] rounded-[32px] bg-[#9855D4] text-[#FFFFFF] text-[16px] font-[500]" style={{ fontFamily: 'Roboto Mono' }}>
+      <button className="flex items-center justify-center w-[93px] md:w-[139.5px] lg:w-[186px] h-[44px] border-[1px] border-[#2E374C] rounded-[32px] bg-[#9855D4] text-[#FFFFFF] text-[10px] md:text-[14px] lg:text-[18px] font-[500]" style={{ fontFamily: 'Roboto Mono' }}>
       How It Works</button>
     </div>
   );
