1b39a28961e712052195c836a9593824cb9217f0
rithnair <rithika@culturefoundrystudios.com>
2025-03-16 01:33:21 -0700

    Add UI changes to about us and HowItWorks

--- frontend/src/app/about-us/page.js
+++ frontend/src/app/about-us/page.js
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState } from 'react';
 import "../globals.css";
-
 import HowItWorks from '../components/HowItWorks';
 
 const AboutUsPage = () => {
@@ -12,18 +11,18 @@ const AboutUsPage = () => {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
             <div className="min-h-screen flex flex-col">
-                <div className="bg-[#D5E9FA]">
+                <div className="bg-blue">
                     <div className="gap-[32px] pt-20 text-center">
-                        <p className="text-[#454F54] text-[18px] font-[500]" style={{ fontFamily: 'Roboto Mono' }}>About Us</p>
-                        <h1 className="text-[#152935] text-[48px] font-[400]" style={{ fontFamily: 'Suez One' }}>The Story Behind IntelliAgents</h1>
+                        <p className="text-light text-[18px] font-[500] roboto-font">About Us</p>
+                        <h1 className="text-dark text-[48px] font-[400] suez-font">The Story Behind IntelliAgents</h1>
                     </div>
                     
                     <div className="flex flex-col-reverse md:grid md:grid-cols-2 w-full items-center justify-between pt-[32px] md:pt-[128px] lg:pt-[128px] pb-[128px] px-10 md:px-20 lg:px-32">
                         {/* Left content */}
-                        <div className="text-[#152935] space-y-5 font-[400] flex flex-col justify-center rounded-lg">
+                        <div className="text-dark space-y-5 font-[400] flex flex-col justify-center rounded-lg">
                             <h2 className=" text-[20px] md:text-[40px] lg:text-[40px] text-center md:text-left lg:text-left " style={{ fontFamily: 'Suez One' }}>What is IntelliAgents?</h2>
-                            <p className="text-[#152935] text-[18px]" style={{ fontFamily: 'Roboto Mono' }}>IntelliAgents is your AI-powered semester planning assistant, designed to help students stay organised and stress-free. With just a few clicks, our smart system extracts key deadlines from your syllabus and syncs them directly to your Google Calendar—no more manual input, missed deadlines, or last-minute scrambles.</p>
-                            <p className="text-[#152935] text-[18px]" style={{ fontFamily: 'Roboto Mono' }}>Whether you're managing multiple courses, extracurriculars, or work commitments, IntelliAgents ensures you have a clear roadmap for your semester, so you can focus on what truly matters—learning, growing, and achieving your goals.</p>
+                            <p className="text-dark text-[18px] roboto-font">IntelliAgents is your AI-powered semester planning assistant, designed to help students stay organised and stress-free. With just a few clicks, our smart system extracts key deadlines from your syllabus and syncs them directly to your Google Calendar—no more manual input, missed deadlines, or last-minute scrambles.</p>
+                            <p className="text-dark text-[18px] roboto-font">Whether you're managing multiple courses, extracurriculars, or work commitments, IntelliAgents ensures you have a clear roadmap for your semester, so you can focus on what truly matters—learning, growing, and achieving your goals.</p>
                         </div>
             
                         {/* Right content */}
@@ -52,23 +51,23 @@ const AboutUsPage = () => {
                         </div>
                                    
                         {/* Right content */}
-                        <div className="text-[#152935] space-y-5 font-[400] flex flex-col justify-center rounded-lg">
+                        <div className="text-dark space-y-5 font-[400] flex flex-col justify-center rounded-lg">
                             <h2 className=" text-[20px] md:text-[40px] lg:text-[40px] text-center md:text-left lg:text-left " style={{ fontFamily: 'Suez One' }}>The IntelliTeam</h2>
-                            <p className="text-[#152935] text-[18px]" style={{ fontFamily: 'Roboto Mono' }}>IntelliAgents is built by a passionate team of students at UBC’s MDIA470 course, in collaboration with Emerging Media Lab. Our team — Valery, Stuti, Iris, Rithika, Leon, Daffa, and Vivian — shares a common goal: making academic planning effortless and efficient.</p>
-                            <p className="text-[#152935] text-[18px]" style={{ fontFamily: 'Roboto Mono' }}>As students, we understand the challenges of juggling assignments, exams, and projects. That’s why we created IntelliAgents—to empower students with smarter, AI-driven tools that simplify semester planning and reduce stress.</p>
-                            <p className="text-[#152935] text-[18px]" style={{ fontFamily: 'Roboto Mono' }}>We're constantly innovating and improving IntelliAgents to serve students better. Try it out today and take control of your semester with ease!</p>
+                            <p className="text-dark text-[18px] roboto-font">IntelliAgents is built by a passionate team of students at UBC’s MDIA470 course, in collaboration with Emerging Media Lab. Our team — Valery, Stuti, Iris, Rithika, Leon, Daffa, and Vivian — shares a common goal: making academic planning effortless and efficient.</p>
+                            <p className="text-dark text-[18px] roboto-font">As students, we understand the challenges of juggling assignments, exams, and projects. That’s why we created IntelliAgents—to empower students with smarter, AI-driven tools that simplify semester planning and reduce stress.</p>
+                            <p className="text-dark text-[18px] roboto-font">We're constantly innovating and improving IntelliAgents to serve students better. Try it out today and take control of your semester with ease!</p>
                         </div>
                     </div>
                 </div>
 
-                <div className="bg-[#D5E9FA]">
+                <div className="bg-blue">
                     <div className="flex flex-col-reverse md:grid w-full items-center justify-center pt-[128px] pb-[128px] px-10">
                         {/* Title and Subtitle */}
                         <div className="space-y-5 font-[400] flex flex-col justify-center items-center rounded-lg text-center">
                             <h2 className="text-[20px] md:text-[40px] lg:text-[40px]" style={{ fontFamily: 'Suez One' }}>
-                                Let us help you with your semester :)
+                                Let us help you with your semester :
                             </h2>
-                            <p className="text-[#152935] text-[18px]" style={{ fontFamily: 'Roboto Mono' }}>
+                            <p className="text-dark text-[18px] roboto-font">
                                 Get started with 3 different methods
                             </p>
                             <HowItWorks />
