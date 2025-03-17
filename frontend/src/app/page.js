66132f2a9d8e75e296a580dd446981b5ab8cf1e1
buyangdaffa <daffaeffendy2911@gmail.com>
2025-03-17 15:14:27 -0700

    removed font

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -14,10 +14,10 @@ const InfinitiScrollLandingPage = () => {
           <div className="flex flex-row w-full h-[1181px] justify-between bg-[#D5E9FA]">
             {/* Left content */}
             <div className="flex flex-col items-center md:items-start lg:items-start justify-center md:pl-16 lg:pl-16 md:pr-16 lg:pr-16">
-              <h1 className="text-[60px] md:text-[90px] lg:text-[120px] text-[#283C69] font-[400]" style={{ fontFamily: 'Suez One' }}>
+              <h1 className="text-[60px] md:text-[90px] lg:text-[120px] text-[#283C69] font-[400] suez-font">
                 IntelliAgents
               </h1>
-              <p className="text-[#505F7C] text-[12px] md:text-[18px] lg:text-[24px] font-[400] text-center md:text-left lg:text-left mb-20" style={{ fontFamily: 'Roboto Mono' }}>
+              <p className="text-[#505F7C] text-[16px] md:text-[20px] lg:text-[24px] font-[400] text-center md:text-left lg:text-left mb-20 roboto-font">
                 The AI agent that organises your schedule... and <br /> keeps it organised.
               </p>
               <div className="flex flex-col space-y-10 mb-8 items-center">
@@ -43,10 +43,10 @@ const InfinitiScrollLandingPage = () => {
           </div>
 
           <div id="how-it-works" className="flex flex-col items-center justify-center w-full h-auto bg-[#FFFFFB]">
-            <p className="text-[#454F54] text-[18px] font-[500] pt-10" style={{ fontFamily: 'Roboto Mono' }}>
+            <p className="text-[#454F54] text-[18px] font-[500] pt-10 roboto-font">
               How It works
             </p>
-            <h2 className="text-[30px] md:text-[45px] font-[400] text-[#152935] mb-10" style={{ fontFamily: 'Suez One' }}>
+            <h2 className="text-[30px] md:text-[45px] font-[400] text-[#152935] mb-10 suez-font">
               Get Started With 3 Methods
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full items-center justify-between">
@@ -67,26 +67,26 @@ const InfinitiScrollLandingPage = () => {
             {/* Right content */}
             <div className="flex flex-col justify-center space-y-10 mb-10 pl-16 pr-16 rounded-lg">
               <div className="flex flex-col space-y-4 text-[#152935]">
-                <div className="w-[36px] h-[36px] bg-[#FFFFFF] rounded-full border-[1px] border-[#152935] flex items-center justify-center font-[600] text-[20px]" style={{ fontFamily: 'Roboto Mono' }}>1</div>
+                <div className="w-[36px] h-[36px] bg-[#FFFFFF] rounded-full border-[1px] border-[#152935] flex items-center justify-center font-[600] text-[20px] roboto-font">1</div>
                 <div>
-                  <h2 className="text-[24px] font-[400]" style={{ fontFamily: 'Suez One' }}>Sign in with Google (recommended)</h2>
-                  <p className="text-[18px] font-[400]" style={{ fontFamily: 'Roboto Mono' }}>Click on the sign-in button with the google icon to link your google calendar, and have your calendar directly updated.</p>
+                  <h2 className="text-[24px] font-[400] suez-font">Sign in with Google (recommended)</h2>
+                  <p className="text-[18px] font-[400] roboto-font">Click on the sign-in button with the google icon to link your google calendar, and have your calendar directly updated.</p>
                 </div>
               </div>
               
               <div className="flex flex-col space-y-4">
-                <div className="w-[36px] h-[36px] bg-[#FFFFFF] rounded-full border-[1px] border-[#152935] flex items-center justify-center font-[600] text-[20px]" style={{ fontFamily: 'Roboto Mono' }}>2</div>
+                <div className="w-[36px] h-[36px] bg-[#FFFFFF] rounded-full border-[1px] border-[#152935] flex items-center justify-center font-[600] text-[20px] roboto-font">2</div>
                 <div>
-                  <h2 className="text-[24px] font-[400]" style={{ fontFamily: 'Suez One' }}>Drag and drop a syllabus without signing in</h2>
-                  <p className="text-[18px] font-[400]" style={{ fontFamily: 'Roboto Mono' }}>Worried about data privacy? Check out our FAQ section below! If you still want to go ahead without signing in, we'll download a file for you to sync up on your calendar.</p>
+                  <h2 className="text-[24px] font-[400] suez-font">Drag and drop a syllabus without signing in</h2>
+                  <p className="text-[18px] font-[400] roboto-font">Worried about data privacy? Check out our FAQ section below! If you still want to go ahead without signing in, we'll download a file for you to sync up on your calendar.</p>
                 </div>
               </div>
 
               <div className="flex flex-col space-y-4">
-                <div className="w-[36px] h-[36px] bg-[#FFFFFF] rounded-full border-[1px] border-[#152935] flex items-center justify-center font-[600] text-[20px]" style={{ fontFamily: 'Roboto Mono' }}>3</div>
+                <div className="w-[36px] h-[36px] bg-[#FFFFFF] rounded-full border-[1px] border-[#152935] flex items-center justify-center font-[600] text-[20px] roboto-font">3</div>
                 <div>
-                  <h2 className="text-[24px] font-[400]" style={{ fontFamily: 'Suez One' }}>Paste text from your syllabus directly</h2>
-                  <p className="text-[18px] font-[400]" style={{ fontFamily: 'Roboto Mono' }}>If you don't have a pdf or document - we still got you! Simply copy and paste your syllabus into our text box.</p>
+                  <h2 className="text-[24px] font-[400] suez-font">Paste text from your syllabus directly</h2>
+                  <p className="text-[18px] font-[400] roboto-font">If you don't have a pdf or document - we still got you! Simply copy and paste your syllabus into our text box.</p>
                 </div>
               </div>
             </div>
@@ -95,7 +95,7 @@ const InfinitiScrollLandingPage = () => {
           </div>
 
           <div className="flex flex-col items-center justify-center w-full h-auto bg-[#D5E9FA] pb-10">
-            <h2 className="text-[30px] md:text-[45px] font-[400] text-[#152935] mb-20 pt-10" style={{ fontFamily: 'Suez One' }}>
+            <h2 className="text-[30px] md:text-[45px] font-[400] text-[#152935] mb-20 pt-10 suez-font">
               Frequently Asked Questions
             </h2>
             <FrequentlyAskedQuestionAccordion />
