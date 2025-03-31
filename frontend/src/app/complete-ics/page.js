1a57a4da1f9cc51cb65d4ca794906a2fe6b96309
rithnair <rithika@culturefoundrystudios.com>
2025-03-31 11:48:14 -0700

    add chatbot to complete-ics page

--- frontend/src/app/complete-ics/page.js
+++ frontend/src/app/complete-ics/page.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import "../globals.css";
 import LoadingAnimation from '../components/LoadingAnimation';
 import Link from 'next/link';
+import Chat from '../components/chat/Chat';
 
 const handleDownload = () => {
   console.log("Download button clicked");
@@ -20,35 +21,41 @@ const handleDownload = () => {
 
 const mainContent = (
   <main className="flex-1 flex justify-center items-center">
-    <div className="flex flex-col items-center justify-center px-4 py-16 text-center w-full">
-      <div className="flex justify-center mb-6">
-        <img src="/images/check.png" alt="Check mark" />
-      </div>
-      
-      <h6 className="mb-4 text-center">Integration Complete!</h6>
-      
-      <div className="text-center mx-auto mb-8">
-        <p className='text-light text-md md:text-xl lg:text-2xl'> 
-          Check your calendar - IntelliAgents has successfully integrated your syllabus into your calendar!
-        </p>
-        <p className='text-light text-md md:text-xl lg:text-2xl mt-2'>
-          Thank you for using IntelliAgents.
-        </p>
+    {/* <div className="flex flex-col items-center justify-center px-4 py-16 text-center w-full"> */}
+      <div className="flex flex-col items-center justify-center px-4 py-16 text-center justify-center w-3/5">
+        <div className="flex justify-center mb-6">
+          <img src="/images/check.png" alt="Check mark" />
+        </div>
+
+        <h6 className="mb-4 text-center">Integration Complete!</h6>
+
+        <div className="text-center mx-auto mb-8">
+          <p className='text-light text-md md:text-xl lg:text-2xl'>
+            Check your calendar - IntelliAgents has successfully integrated your syllabus into your calendar!
+          </p>
+          <p className='text-light text-md md:text-xl lg:text-2xl mt-2'>
+            Thank you for using IntelliAgents.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center space-y-5 mt-8">
+          <button className="px-8 py-3 font-bold button button-rounded w-64"
+            onClick={handleDownload} >
+            Download .ics File
+          </button>
+
+          <Link href={{ pathname: '/' }}>
+            <button className="px-8 py-3 button button-blue button-rounded font-bold w-64">
+              Return Home
+            </button>
+          </Link>
+        </div>
       </div>
 
-      <div className="flex flex-col items-center justify-center space-y-5 mt-8">
-        <button className="px-8 py-3 font-bold button button-rounded w-64"
-          onClick={handleDownload} >
-          Download .ics File
-        </button>
-        
-        <Link href={{ pathname: '/' }}>
-          <button className="px-8 py-3 button button-blue button-rounded font-bold w-64">
-            Return Home
-          </button>
-        </Link>
+      <div className="flex w-2/5 justify-center items-center hidden lg:flex">
+        <Chat />
       </div>
-    </div>
+    {/* </div> */}
   </main>
 );
 
